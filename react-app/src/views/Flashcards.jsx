b49c0cd5df4f4b9da52cafa41662f382b1682b39
Brooke Pariyar <brookepariyar@gmail.com>
2024-09-24 12:13:32 -0700

    shows all cards, but can click them

--- react-app/src/views/Flashcards.jsx
+++ react-app/src/views/Flashcards.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Button } from 'bootstrap';
 
 export default function Flashcards() {
     const pathname = window.location.pathname;
     const segment = pathname.substring(pathname.lastIndexOf('/') + 1);
 
     const [cards, setCards] = useState();
+    const [x, setX] = useState(true);
+    let [index, setIndex] = useState(1);
 
     useEffect(() => {
         fetch('http://localhost:8080/flashcard/cards/' + segment)
@@ -17,13 +20,23 @@ export default function Flashcards() {
     return (
         <>
             <h1>Flashcards</h1>
+            <button type="button"
+                        onClick={() => {
+                            if(index <= 1) {
+                                index = 5;
+                            } else {
+                                setIndex(index - 1);
+                            }
+                        }}>Back</button>
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-2 g-4">
-                {cards?.map(card => (
+                    {cards?.map(card =>
                     <div className="col" key={card.flashcardId}>
                         <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{card.frontData}</h5>
-
+                            <div className="card-body" onClick={() => { setX(!x); }}>
+                                {x ?
+                                <h5 className="card-title">{card.frontData}</h5> :
+                                <h5 className="card-title">{card.backData}</h5>
+                                }
                             </div>
                             <div className="card-footer text-body-secondary text-center">
                                 <Link to={`/delete/${card.flashcardId}`} className="btn btn-danger me-2">Delete</Link>
@@ -31,8 +44,16 @@ export default function Flashcards() {
                             </div>
                         </div>
                     </div>
-                ))}
+                )}
             </div>
+            <button type="button"
+                        onClick={() => {
+                            if(index >= 5) {
+                                index = 1;
+                            } else {
+                                setIndex(index + 1);
+                            }
+                        }}>Next</button>
         </>
     );
 }
